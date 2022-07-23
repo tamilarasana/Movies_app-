8fc46a5df5e89915cd8eb322fef415cab182efde
Tamilarasan Annamalai <tamilarasanars@gmail.com>
2022-07-23 12:39:51 +0530

    pagination - Handling Page Changes

---
+++ src/components/common/like.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+const Like = (props) => {
+    let classes = "fa fa-heart";
+    if (!props.Liked) classes += "-o";
+    return (
+      <i
+        onClick={props.onClick}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden
+      />
+    );
+}
+
+// class Like extends Component {
+//   render() {
+//     let classes = "fa fa-heart";
+//     if (!this.props.Liked) classes += "-o";
+//     return (
+//       <i
+//         onClick={this.props.onClick}
+//         style={{ cursor: "pointer" }}
+//         className={classes}
+//         aria-hidden
+//       />
+//     );
+//   }
+// }
+
+export default Like;
