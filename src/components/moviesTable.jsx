764c5b1af3d8119b02b982c36f6a1ba044155537
Tamil <tamildef@gmail.com>
2022-07-27 14:57:11 +0530

    Sorting-Extracting Cell Content

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,6 +2,7 @@
 import Like from "./common/like";
 import React, { Component } from "react";
 import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 
 class MoviesTable extends Component {
   columns = [
@@ -22,6 +23,7 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
+        <TableBody  data = {movies} />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
