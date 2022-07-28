d1bdb8c0cae77e18ae53261f2d40087ab5ba2777
Tamil <tamildef@gmail.com>
2022-07-28 16:12:38 +0530

    Adding Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Movies from './components/movies';
+import "./App.css";
+import Movies from "./components/movies";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Customers from "./components/customers";
+import Rental from "./components/rental";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
-    <main className='container'>  
-        <Movies />
+    <main className="container">
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rental" component={Rental} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/"  exact to="/movies" />
+        <Redirect  to="/not-found" />
+        {/* <Movies /> */}
+      </Switch>
     </main>
   );
 }
 
 export default App;
- 
--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TableHeader from './tableHeader';
 import TableBody from './tableBody';
 
-const Table = ( {columns, sortColumn, onSort, data}) => {
+const Table = ( {columns, sortColumn, onSort, data} ) => {
     // const {columns, sortColumn, onSort, data} = props;
     return (  
         <table className="table">
