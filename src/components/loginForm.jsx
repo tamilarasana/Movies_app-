cf1e91f650f7cea500c1beadb8ace158b709d3a0
Tamil <tamildef@gmail.com>
2022-07-28 19:44:26 +0530

    Controlled Elements

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,37 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LoginForm extends Component {
-    username = React.createRef();
+  // username = React.createRef();
 
-    // componentDidMount() {
-    //     this.username.current.focus();
-    // }
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const username = this.username.current.value;
-        console.log(username);
-        
-    };
+  // componentDidMount() {
+  //     this.username.current.focus();
+  // }
 
-    render() { 
-        return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="username" >Username</label>
-                        <input autoFocus  ref={this.username} id="username" type="text" className="form-control" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Username</label>
-                        <input id="password" type="text" className="form-control" />
-                    </div>
-                    <button className="btn btn-primary">Login</button>
-                </form>
-            </div>
-        );
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const username = this.username.current.value;
+    console.log(username);
+  };
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              id="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
