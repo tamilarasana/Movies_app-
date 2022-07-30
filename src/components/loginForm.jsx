c4070f23f24379bb0351f0f2c17b739bd2a4ab7a
Tamilarasan A <tamilarasanars@gmail.com>
2022-07-30 12:00:13 +0530

    Extracting Helper Rendering

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Input from "./common/input";
 import  Joi  from "joi-browser";
 import Form from "./common/form";
 
@@ -25,27 +24,13 @@ class LoginForm extends Form {
     console.log("Submitted");
   }
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.value}
-            label="Username"
-            onChange={this.handleChange}
-            error = {errors.username}
-          />
-
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error = {errors.password}
-          />
-          <button  disabled={this.validate()} className="btn btn-primary">Login</button>
+         {this.renderInput('username', 'Username')}
+         {this.renderInput('password', 'Password', 'password')}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
