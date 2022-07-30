b16a72e392409a39b2c9b916723c787e1ca702ff
Tamilarasan A <tamilarasanars@gmail.com>
2022-07-30 10:43:51 +0530

    Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import  Joi  from "joi-browser";
 class LoginForm extends Component {
   // username = React.createRef();
 
@@ -14,7 +15,17 @@ class LoginForm extends Component {
   // componentDidMount() {
   //     this.username.current.focus();
   // }
+
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required()
+  }
   validate = () => {
+    const result  = Joi.validate(this.state.account, this.schema,{
+      abortEarly:false
+    });
+    console.log(result);
+
     const errors = {};
     const {account} = this.state;
     if(account.username.trim() === '')
